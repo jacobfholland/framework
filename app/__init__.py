b89472a4ad91ac24d51548b159cb67f1acace5eb
Jacob Holland <jacobfholland@gmail.com>
2023-12-30 20:40:04 +0000

    [WIP] App build

--- app/__init__.py
+++ app/__init__.py
@@ -4,22 +4,18 @@ import os
 import sys
 
 
-def create_app(conf, logger):
+def create_app():
     try:
         from app.app import Application
-        return Application(conf, logger)
+        return Application()
     except Exception as e:
-        logging.error(f"Failed to import Application class: {e}")
+        logging.error(f"Failed to build application: {e}")
         sys.exit(1)
 
 
 # try:
 logger = log.logger
-conf = config.conf
-for k, v in conf.__dict__.items():
-    logger.debug(f"{k}: {v}")
-app = create_app(conf, logger)
+
+app = create_app()
 logger.info(f"Core application built successfully")
 # except Exception as e:
-#     logging.error(f"Failed to build Application: {e}")
-#     sys.exit(1)
--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
+from app.config import conf
+from app.log import logger
+
+
 class Application:
-    def __init__(self, conf, logger) -> None:
+    def __init__(self) -> None:
         self.conf = conf
         self.logger = logger
